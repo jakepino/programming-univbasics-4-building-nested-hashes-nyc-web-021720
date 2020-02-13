3f01c6e67e940d1af176d8897e5f829f2952cdfc
Jake Pino <jakepino@yahoo.com>
2020-02-13 23:53:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -60,9 +60,9 @@ def monopoly_with_fourth_tier
       :names => 
       {
         :reading_railroad => {"morgage_value" => "$100"},
-        :pennsylvania_railroad => {"morgage_value" => "$100"},
-        :b_and_o_railroad => {"morgage_value" => "$100"},
-        :shortline_railroad => {"morgage_value" => "$100"}
+        :pennsylvania_railroad => {"morgage_value" => "$200"},
+        :b_and_o_railroad => {"morgage_value" => "$300"},
+        :shortline_railroad => {"morgage_value" => "$800"}
       }
         
   }
